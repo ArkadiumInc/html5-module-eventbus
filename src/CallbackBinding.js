ed5f49aa11f3773fa1954f60c40d92e4986b8840
Alexey Shmakov <razzeeyy@gmail.com>
2016-04-27 21:12:07 +0300

    add callback binding class, also trying to push to test the write access permission

---
+++ src/CallbackBinding.js
@@ -0,0 +1,31 @@
+function CallbackBinding(callback, context) {
+    "use strict";
+    this._callback = callback;
+    this._context = context;
+}
+module.exports = CallbackBinding;
+
+CallbackBinding.prototype.destroy = function() {
+    "use strict";
+    this._callback = null;
+    this._context = null;
+}
+
+CallbackBinding.prototype.execute = function(/*arguments*/) {
+    "use strict";
+    this._callback.apply(this._context, arguments);
+}
+
+CallbackBinding.prototype.equals = function(/*[otherCallback] | [callback, context]*/) {
+    "use strict";
+    var otherCallback = null;
+    var otherContext = null;
+    if(arguments.length === 2) {
+        otherCallback = arguments[0];
+        otherContext = arguments[1];
+    } else {
+        otherCallback = arguments[1]._callback;
+        otherContext = arguments[1]._context;
+    }
+    return this._callback === otherCallback && this._context === otherContext;
+}
